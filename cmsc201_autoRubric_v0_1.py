953e1f7f72cb133575ad693ea5bd8145a352dd4c
Akshay Peshave <axxe@localhost.localdomain>
2015-10-30 11:10:55 -0400

    v0.2.1
    Updated un-entered grade character from '@' to '&'

--- cmsc201_autoRubric_v0_1.py
+++ cmsc201_autoRubric_v0_1.py
@@ -1,6 +1,6 @@
 ##################################################################################
 # Script : cmsc201_autoRubric_v0_2.py [CMSC201 Rubric Autocomplete (v0.2)]
-# Author : Akshay Peshave (peshave1@umbc.edu)
+# Author : Akshay Peshave (peshave1umbc.edu)
 # Desc	 : This scripts autocompletes the rubric for a given
 #	   student including computation of total points. 
 #	   The graders/TAs need only fill out those entries of the 
@@ -12,14 +12,9 @@
 #		python [script_name] [student_id]
 #	   E.g. python cmsc201_autoRubric_v0_1.py peshave1
 # 
-# Revisions in v0.2:
-#	1. Improved regex matching for rubric rows to support fractional values
-#	   for grades.
-#	2. Minor code refactoring.
-# Upcoming revisions:
-#	1. execute script once in homework/lab folder to 
-#	   autocomplete rubrics in all student folders available 
-#	   in the hw/lab folder.
+# Revisions in v0.2.1:
+#	1. Changed un-entered grade character from '@' to '&' 
+#	   as per new rubric format
 ##################################################################################
 
 import io
@@ -34,7 +29,7 @@ student="./"+sys.argv[1]
 with open(student + "/rubric_new.txt", "wb") as newRubricFile:
     with open(student + "/rubric.txt", "rb") as rubricFile:
         for line in rubricFile:
-            if str(line).startswith("@"): # replace '@' with full points for that entry in the rubric
+            if str(line).startswith("&"): # replace '&' with full points for that entry in the rubric
                 entryLength = len(line.split(" ")[0].split("/")[0])
                 line = line.split(" ")[0].split("/")[1] + line[entryLength:]
 
